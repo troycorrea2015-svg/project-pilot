1e7081c73744fce82142a5bae35275090bf5db5d
troycorrea2015-svg <troycorrea2015@gmail.com>
2026-07-11 13:28:11 -0400

    Delete page.js

--- page.js
+++
@@ -1,3 +0,0 @@
-import "./globals.css";
-export const metadata={title:"Project Pilot",description:"Home project planning and permit guidance"};
-export default function RootLayout({children}){return <html lang="en"><body>{children}</body></html>}
